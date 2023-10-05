eeeb3d0bef6486eb14de8eca048db811cc066af5
dmolloc1 <dmolloc@unsa.edu.pe>
2023-10-05 11:41:51 -0500

    Implementacion de la logica a array list , error IndexOutOfBoundsException

--- Laboratorio6/VideoJuego2.java
+++ Laboratorio6/VideoJuego2.java
@@ -2,12 +2,15 @@ import java.util.*;
 //Laboratorio A Fundamentos 2
 //Autor: Mollo Chuquicaña Dolly Yadhira
 public class VideoJuego2 {
-    static List < List< Soldado > > ejercito = new ArrayList< List<Soldado> >(1);
+    static ArrayList <ArrayList <Soldado>> tablero = new ArrayList <>();
     public static void main(String[] args) {
-    	Soldado[] ejercito =  creandoEjercito();
+    	ArrayList <Soldado> ejercito = new ArrayList <Soldado>(1);
+        for(Soldado a: creandoEjercito()){
+            ejercito.add(a);
+        }
 	    System.out.println("		~TABLERO~");
-	    mostrarTablero(tablero);
-	    mayorNivelVida(ejercito).mostrar();
+	    mostrarTablero();
+/*	    mayorNivelVida(ejercito).mostrar();
 	    System.out.println("\nEl total de nivel de vida del ejercito es :" + totalNivelVida(ejercito));
 	    System.out.println("El promedio de nivel de vida del ejercito es :" + promedioNivelVida(ejercito));
 	    System.out.println("\n~~~ Nivel de vida de los soldados ~~~");
@@ -21,44 +24,49 @@ public class VideoJuego2 {
         Soldado[] ejercito2 = creandoEjercito();
         ordenarPorNivelSelección(ejercito2);
 	    System.out.println("\n~~~ Soldados ordenados Metodo de Selección ~~~");
-        mostrarSoldados(ejercito2);
+        mostrarSoldados(ejercito2);*/
     }
 
     public static ArrayList<Soldado> creandoEjercito() {
-        List <Soldado> listEjercito = new ArrayList <Soldado>(1);
+        ArrayList <Soldado> listEjercito = new ArrayList <Soldado>();
     	int nivelR = 0, filaR = 0, columnaR = 0;
-	    for (int i = 0; i <(int) (Math.random() * 10 + 1); i++) {
-            listEjercito.add(new Soldado());
-            listEjercito.get(i).setNombre("Soldado_" + (i + 1));
-            listEjercito.get(i).setNivelDeVida((int) (Math.random() * 5 + 1));
+	    for (int i = 0; i < (Math.random()*10) + 1; i++) {
+            Soldado nuevo = new Soldado();
+            nuevo.setNombre("Soldado_" + (i + 1));
+            nuevo.setNivelDeVida((int) (Math.random() * 5 + 1));
             // Este ciclo nos permitirá comprobar que los valores generados no coincidan con uno ya existente
             boolean posicionValida = false;
             while (!posicionValida) {
                 filaR = (int) (Math.random() * 9);
                 columnaR = (int) (Math.random() * 9);
-                if (rellenarTablero(filaR, columnaR, soldados[i])) {
+                if (rellenarTablero(filaR, columnaR,nuevo)) {
                     posicionValida = true;
                 }
             }
-            soldados[i].setFila(filaR + 1);
-            soldados[i].setColumna((char) (columnaR + 65));
+            nuevo.setFila(filaR + 1);
+            nuevo.setColumna((char) (columnaR + 65));
+            listEjercito.add(nuevo);
         }
-	return soldados;
+
+	return listEjercito;
     }
 
     public static boolean rellenarTablero(int fila, int columna, Soldado sol) {
-        if (tablero[fila][columna] == null) {
-            tablero[fila][columna] = sol;
+         if (tablero.get(fila) == null) {
+            tablero.set(fila, new ArrayList<>());
+        }
+        if (tablero.get(fila).get(columna) == null) {
+            tablero.get(fila).set(columna, sol);
             return true;
         }
         return false;
     }
 
-    public static void mostrarTablero(Soldado[][] ejercito){
-    	for(int i = 0; i < ejercito.length; i++){
+    public static void mostrarTablero(){
+    	for(int i = 0; i < tablero.size(); i++){
 	    	String fila = "|";
-	    	for(int j = 0; j < ejercito[i].length; j++){
-	    		if(ejercito[i][j] == null){
+	    	for(int j = 0; j < tablero.get(i).size(); j++){
+	    		if(tablero.get(i).get(j) == null){
 					fila = fila +" - " + "|"; 
 				}else{
 					fila = fila + " S " + "|";
@@ -67,7 +75,7 @@ public class VideoJuego2 {
 	    	System.out.println(fila);
 		}
     }
-
+/*
     public static Soldado mayorNivelVida(Soldado[] soldados){
 		Soldado soldMayor = soldados[0];
 
@@ -124,4 +132,7 @@ public class VideoJuego2 {
             System.out.println("---------------------------------\n"+n.mostrar());
         }
     }
+    
+  */
 }
+
