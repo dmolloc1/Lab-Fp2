1d5f616ffd34f44f4d20dc45da04dde4604ac630
dmolloc1 <dmolloc@unsa.edu.pe>
2023-10-21 21:33:54 -0500

    Algunos ajustes

--- Laboratorio11/Soldado.java
+++ Laboratorio11/Soldado.java
@@ -3,29 +3,29 @@ public class Soldado {
 	private String nombre;
 	private int nivelVida, fila;
 	private char columna;
-    private String color;
+	private String color;
     //nuevos atributos
-    private int nivelAtaque, nivelDefensa, vidaActual, velocidad;
-    private String actitud;
-    private boolean vive;
+    	private int nivelAtaque, nivelDefensa, vidaActual, velocidad;
+    	private String actitud;
+    	private boolean vive;
 
-    public Soldado(String nombre, int nivelVida, int nivelAtaque, int nivelDefensa, int velocidad){//Atributos de batalla
-        this.nombre = nombre;
-        this.nivelVida = nivelVida;
-        this.nivelAtaque = nivelAtaque;
-        this.nivelDefensa = nivelDefensa;
-        this.velocidad = velocidad;
-    }
+    	public Soldado(String nombre, int nivelVida, int nivelAtaque, int nivelDefensa, int velocidad){//Atributos de batalla
+        	this.nombre = nombre;
+        	this.nivelVida = nivelVida;
+        	this.nivelAtaque = nivelAtaque;
+        	this.nivelDefensa = nivelDefensa;
+        	this.velocidad = velocidad;
+    	}
 
-    public Soldado(int fila, char columna ){//Atributos de ubicacion y nombre
-        this.fila = fila;
-        this.columna = columna;
-    }
+    	public Soldado(int fila, char columna ){//Atributos de ubicacion y nombre
+        	this.fila = fila;
+        	this.columna = columna;
+    	}
 
-    public Soldado(String actitud, boolean vive){// Atributos de estado
-        this.actitud = actitud;
-        this.vive = vive;
-    }
+    	public Soldado(String actitud, boolean vive){// Atributos de estado
+           	this.actitud = actitud;
+        	this.vive = vive;
+    	}
 
 	public void setNivelDeVida(int num){
 		nivelVida = num;
@@ -38,16 +38,18 @@ public class Soldado {
 	public void setColumna(int colum){
 		columna = (char)(colum + 65);
 	}
-    public void setVive(boolean vivir){
-        vive = vivir;
-    }
-    public void setActitud (String a){
-        actitud = a ;
-    }
+    	
+	public void setVive(boolean vivir){
+        	vive = vivir;
+    	}
+    	
+	public void setActitud (String a){
+    		actitud = a ;
+    	}
 
 	public String getNombre(){
 		return nombre;
-	}
+	}	
 
 	public int getNivelDeVida(){
 		return nivelVida;
@@ -56,16 +58,34 @@ public class Soldado {
 	public int getFila(){
 		return fila;
 	}
-
+	
 	public char getColumna(){
 		return columna;
 	}
-    public boolean getVive(){
-        return vive;
-    }
-    public String getActitud(){
-        return actitud;
-    }
+   
+	public boolean getVive(){
+        	return vive;
+    	}
+    	
+	public String getActitud(){
+        	return actitud;
+    	}
+
+	public void setColor(String color){
+		this.color = color; 
+	}
+
+   	public String getColor(){
+       		return  color;
+	}
+    	
+	public void setVidaActual(int vidaActual){
+        	this.nivelVida = vidaActual;
+    	}
+    	
+	public int getVidaActual(){
+        	return nivelVida;
+    	}
 
 //Metodos
 	public String mostrar(){
@@ -73,67 +93,52 @@ public class Soldado {
             + nivelVida + "\n	-Fila/Columna: "+ fila + " / "+ columna +"\nEstado: "+ actitud; 	
 	}
 
-  	public void setColor(String color){
-      	this.color = color; 
-   	}
+	public void atacar(boolean gano){
+        	this.velocidad += 1;
+        	this.actitud = "Atacar";
+        	this.nivelVida += 1;
+        	if(!gano){
+        	    morir();
+        	    this.vive = false;
+        	}
+    	}
 
-   	public String getColor(){
-       	return  color;
-   	}
+    	public void defender(){
+        	this.velocidad = 0;
+        	this.actitud = "Defender";
+    	}
 
-    public void atacar(boolean gano){
-        this.velocidad += 1;
-        this.actitud = "Atacar";
-        this.nivelVida += 1;
-        if(!gano){
-            morir();
-            this.vive = false;
-        }
-    }
+    	public void huir (){
+        	this.velocidad += 2;
+        	this.actitud = "Huir";
+    	}
 
-    public void defender(){
-        this.velocidad = 0;
-        this.actitud = "Defender";
-    }
+    	public void retroceder(){
+        	if(this.velocidad > 0){
+        	    this.velocidad = 0;
+        	    this.actitud = "Defender";
+        	    this.defender();
+        	}
+        	else{ 
+        	    this.velocidad = this.velocidad - 1;
+        	}
+    	}
 
-    public void huir (){
-        this.velocidad += 2;
-        this.actitud = "Huir";
-    }
+    	public void avanzar(int fila, int columna){
+        	this.velocidad += 1;
+        	setFila(fila);
+        	setColumna(columna);
+    	}
 
-    public void retroceder(){
-        if(this.velocidad > 0){
-            this.velocidad = 0;
-            this.actitud = "Defender";
-            this.defender();
-        }
-        else{ 
-            this.velocidad = this.velocidad - 1;
-        }
-    }
+    	public void serAtacado(boolean perdio){
+        	if(!perdio){
+            	this.nivelVida += 1;
+        	}
+        	else morir();
+    	}
 
-    public void avanzar(int fila, int columna){
-        this.velocidad += 1;
-        setFila(fila);
-        setColumna(columna);
-    }
-
-    public void serAtacado(boolean perdio){
-        if(!perdio){
-            this.nivelVida += 1;
-        }
-        else{morir();}
-    }
-
-    public void morir(){
-        this.vive = false;
-    }
-
-    public void setVidaActual(int vidaActual){
-        this.nivelVida = vidaActual;
-    }
-    public int getVidaActual(){
-        return nivelVida;
-    }
+    	public void morir(){
+        	this.vive = false;
+    	}
 }
 
