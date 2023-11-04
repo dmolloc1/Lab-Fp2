29fa496fdc23e57993d32a41e7245b34514644fa
dmolloc1 <dmolloc@unsa.edu.pe>
2023-11-03 21:18:04 -0500

    Correciones en la clase ejercito y reino

--- Laboratorio14/Ejercito.java
+++ Laboratorio14/Ejercito.java
@@ -1,17 +1,21 @@
 import java.util.*;
 public class Ejercito{
     public static final int MAX_SIZE = 10;
+    private static int ejercitoscantidad;
+    private static int totalEjercitos;
     private static Reino reino;
     private ArrayList <Soldado> army ;
-    
     public Ejercito(){
         this.army = new ArrayList <>();
     }
     public void reclutar(Soldado sold){
         this.army.add(sold);
     }
-    public static  Reino retornarEjercitos(){
-        return reino;
+    public void setReino(Reino miReino){
+        this.reino = miReino;
+    } 
+    public Reino getReino(){
+        return this.reino;
     }
     
 }
