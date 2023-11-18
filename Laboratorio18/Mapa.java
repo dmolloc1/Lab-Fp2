a283d8304a49e20abb5ae7c632f9ad8e6e4e9dd3
dmolloc1 <139632142+dmolloc1@users.noreply.github.com>
2023-11-17 19:37:31 -0500

    Algunos detalles Mapa.java

--- Laboratorio18/Mapa.java
+++ Laboratorio18/Mapa.java
@@ -39,20 +39,7 @@ public class Mapa{
                 break;
         }
     }
-    public void bonificación(ArrayList <Ejercito> reino){
-        Ejercito e = reino.get(0);
-        for(int i = 0; i < e.getBono().length(); i++){
-            if(e.getBono().charAt(i) == this.bonificación) {Ejercito.bono(reino);}
-        }
-        
-    }
-    public boolean posiciónValida(int fila, int columna, Ejercito eje) {
-        if (this.mapa[fila][columna] == null) {
-            this.mapa[fila][columna] = eje;
-            return true;
-        }
-        return false;
-    }
+    
     public boolean posiciónValida(int fila, int columna, Soldado sold) {
         if (this.campo[fila][columna] == null) {
             this.campo[fila][columna] = sold;
@@ -60,46 +47,7 @@ public class Mapa{
         }
         return false;
     }
-    public void mostrar(){
-        System.out.println("* |   A   |   B   |   C   |   D   |   E   |   F   |   G   |   H   |   I   |   J   |\n"+
-                           "-----------------------------------------------------------------------------------");
-        for(int i = 0; i < this.mapa.length; i++){
-                String fila = "|";
-                for(int j = 0; j <this.mapa[i].length; j++){
-                    if(this.mapa[i][j] == null){ fila = fila +"  ---  " + "|";
-                    }else{
-                        if(this.mapa[i][j].size() > 9){
-                            fila += " " + this.mapa[i][j].getColor() + this.mapa[i][j].size() + "-";
-                        }
-                        else{fila += "  " + this.mapa[i][j].getColor() + this.mapa[i][j].size() + "-";}
-                        
-                        if(this.mapa[i][j].totalNivelVida() > 9 ){
-                            fila += this.mapa[i][j].totalNivelVida()  + "\u001B[0m |";
-                        }
-                        else {
-                            fila += " " + this.mapa[i][j].totalNivelVida()  + "\u001B[0m |";
-                        }
-                    }
-                }
-                if(i == 9){
-                    System.out.println((i + 1)  + fila);
-                }
-                else System.out.println((i + 1) +" " + fila);
-        }
-    }
-    public void rellenar(ArrayList <Ejercito> ejercito){
-        int fila = 0, columna = 0;
-        for(Ejercito e : ejercito){
-            boolean posicionValida = false;
-            while (!posicionValida) {
-                fila = (int) (Math.random() * 9);
-                columna = (int) (Math.random() * 9);
-                posicionValida = this.posiciónValida(fila, columna, e);         
-            }
-            e.setFila(fila);
-            e.setColumna(columna);
-        }
-    }
+    
     public void rellenar(Ejercito ejercito){ //Metodo para rellenar pero de soldados
         int fila = 0, columna = 0;
         for(Soldado e : ejercito.getSoldados()){
@@ -130,92 +78,5 @@ public class Mapa{
                 }
                 else System.out.println((i + 1) +" " + fila);
         }
-    }
-    
-   //Metodos para mover el tablero de ejercitos
-    public void jugar(int ejercito, ArrayList <Ejercito> reino1, ArrayList <Ejercito> reino2){
-       
-	    Scanner sc = new Scanner (System.in);
-        System.out.print("Posición del ejercito a mover:" + "\nFila: ");
-        int fila = sc.nextInt() - 1;
-        System.out.print("Columna: ");
-        int columna = Integer.valueOf(sc.next().toUpperCase().charAt(0)) - 65;
-        while(this.mapa[fila][columna] == null){
-            System.out.print("Posición invalida");
-            System.out.print("Posición del ejercito a mover:" + "\nFila: ");
-            fila = sc.nextInt() - 1;
-            System.out.print("Columna: ");
-	        columna = Integer.valueOf(sc.next().toUpperCase().charAt(0)) - 65;
-        }
-	    boolean posValida = true;
-        while (posValida){
-        	System.out.print("\nDirección (I = ⬅ , D = ➡ , A = ⬆ , B = ⬇ , DIS = ⬉ , DII = ⬋, DDS = ⬈, DDI = ⬊ ):");
-	     	String dir = sc.next();
-	     	posValida = this.moverEjercito(fila, columna, dir, ejercito, reino1, reino2);
-        }
-        
-        this.mapa[fila][columna]= null;
-    }
-    public boolean moverEjercito(int fila, int columna, String comando, int ejercito, ArrayList <Ejercito> r1, ArrayList <Ejercito> r2){	
-    	Ejercito eje = this.mapa[fila][columna];
-        switch (comando) {
-            case "A":
-                fila = fila - 1;
-                if(fila < 0){return true;}
-                break;
-            case "B":
-                fila = fila + 1;
-                if(fila > 9){return true;}
-                break;
-            case "I":
-                columna =  columna - 1;
-                if(columna  < 0){return true;}
-                break;
-            case "D":
-                columna = columna + 1;
-                if(columna > 9){return true;}
-                break;
-            case "DII":
-                fila = fila + 1;
-                columna =  columna - 1;
-                if(fila > 9 || columna < 0){return true;}
-		        break;
-            case "DDI":
-                fila = fila + 1;
-                columna =  columna + 1;
-                if(fila > 9 || columna > 9){return true;}
-                break;
-            case "DIS":
-                fila = fila - 1;
-                columna =  columna - 1;
-                if(fila < 0 || columna < 0){return true;}
-                break;
-            case "DDS":
-                fila = fila - 1;
-                columna =  columna + 1;
-                if(fila < 0 || columna > 9){return true;}
-                break;
-        }
-        System.out.println(fila+ " "+ columna);
-        this.cambiarPosición(eje , fila , columna, ejercito, r1, r2);
-        return false;
-    }
-
-    public void cambiarPosición(Ejercito eje, int fila, int columna, int ejercito, ArrayList <Ejercito> reino1, ArrayList <Ejercito> reino2){
-        Ejercito enemigo = this.mapa[fila][columna];
-        if (enemigo == null) {
-            this.mapa[fila][columna] = eje;
-        }
-
-      /* else{
-            if(VideoJuego5.ganador(eje, enemigo, fila, columna, ejercito)){
-                mapa[fila][columna] = eje;
-                eje.avanzar(fila, columna);
-            }
-            
-        }*/
-    } 
-            
-            
-            
+    }    
 }
