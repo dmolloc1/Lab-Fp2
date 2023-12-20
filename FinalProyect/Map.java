ca51eb26b021241640be7680a2d47f2c79b1c6b3
dmolloc1 <dmolloc@unsa.edu.pe>
2023-12-20 08:27:07 -0500

    Se creo las clases bases

---
+++ FinalProyect/Map.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Map{
+    private Land land;//es un picture
+    private ArrayList<Land> field;
+    public Map(){ 
+    }
+
+    private void builtMap(){
+        //Logica para unir el arrat de Picture
+    }
+    private void levelUp(Castle c, Land l){
+        //Logicca para agregar un item
+    }
+}
